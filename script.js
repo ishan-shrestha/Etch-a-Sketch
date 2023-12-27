121033a43dcc3a7037573de7e636f24573b03824
Ishan Narayan Shrestha <ishaanstha18@gmail.com>
2023-12-27 11:07:52 +0545

    update grid with funcationality, clear button and hover

--- script.js
+++ script.js
@@ -5,32 +5,47 @@ const clearBtn = document.querySelector(".clear-button");
 const rgbBtn = document.querySelector(".RGB");
 let numGrid = 16;
 
-displayText.textContent = `The size of grid ${numGrid} & ${numGrid}`;
-
+displayText.textContent = `The size of grid: ${numGrid} & ${numGrid}`;
 function createDiv() {
-    for (let i = 0; i < (numGrid * numGrid); i++) {
-        const square = document.createElement("div");
-        square.className = "square";
-        container.appendChild(square);
+    for (let i = 0; i < numGrid; i++){
+        const row = document.createElement("div");
+        row.className = "row";
+        container.appendChild(row);
 
-        square.addEventListener("mousemove", () => {
-            square.style.backgroundColor = "red";
-        })
+        for (let j = 0; j < numGrid; j++) {
+            const column = document.createElement("div");
+            column.className = "column";
+            row.appendChild(column);
 
-        clearBtn.addEventListener("click", () => {
-            square.style.backgroundColor = "white"; 
-         })
+            column.addEventListener("mousemove", () => {
+                column.style.backgroundColor = "red";
+            })
+
+            clearBtn.addEventListener("click", () => {
+                column.style.backgroundColor = "white";
+            })
+        }
     }
 }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+createDiv();
 
 chnageBtn.addEventListener("click", () => {
     const userInput = prompt("Enter a number from 1 to 80");
     if (userInput == null || userInput > 80 || userInput <= 0 || isNaN(userInput)) {
         alert("Re-enter the number between 1 to 80");
         return;
+    } else {
+        removeAllChildNodes(container);
+        numGrid = userInput;
+        displayText.textContent = `The size of grid: ${numGrid} & ${numGrid}`;
+        createDiv();
     }
-
-    createDiv(userInput);
 })
-
-createDiv();
